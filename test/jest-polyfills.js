ebc6777a7ed37ed668a689356a8abbefe9707bb4
Oz Ramos <labofoz@gmail.com>
2018-11-14 09:01:06 -0800

    Adds unit tests

---
+++ test/jest-polyfills.js
@@ -0,0 +1,53 @@
+/**
+ * A suite of polyfill togglers for testing with jest
+ */
+
+/**
+ * Catch window.alert
+ */
+window.alert = function () {}
+
+/**
+ * Suppress known error messages
+ */
+window.consoleError = console.error
+console.error = function (message, ...args) {
+  if (message.startsWith('ERROR: This browser does not support webcams, please try another browser...like Google Chrome!')
+    || message.startsWith('Unexpected character')
+    ) {
+    console.warning('Uncaught Error', ...args)
+  } else {
+    window.consoleError(message, ...args)
+  }
+}
+
+module.exports = {
+  mediaDevices: {
+    support () {
+      window.HTMLMediaElement.prototype.load = () => {}
+      window.HTMLMediaElement.prototype.play = () => {}
+      window.HTMLMediaElement.prototype.pause = () => {}
+      window.HTMLMediaElement.prototype.addTextTrack = () => {}
+      window.HTMLMediaElement.prototype.srcObject = {
+        getTracks: () => [{stop: jest.fn()}]
+      }
+      navigator.mediaDevices = {
+        getUserMedia: function () {
+          return {
+            then: function () {
+              return {
+                catch: function () {}
+              }
+            }
+          }
+        }
+      }
+    },
+    unsupport () {navigator.mediaDevices = null}
+  },
+
+  WebGL: {
+    support () {window.WebGLRenderingContext = true},
+    unsupport () {window.WebGLRenderingContext = false}
+  }
+}
