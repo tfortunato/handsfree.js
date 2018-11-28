3d9768c8094c80f9b27cfc9b99436c579a2cebc9
Oz Ramos <labofoz@gmail.com>
2018-11-28 15:50:26 -0800

    #13 Add info to stats

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -6,20 +6,29 @@
           v-card(light)
             v-card-title
               h2 Settings
+            v-card-text
+              v-alert(type='info' :value='true') The following settings will be saved to your browser's localStorage.
 
         v-flex(xs12 md6 lg4)
           v-card.mb-2(light)
-            v-card-text.statsjs(ref='stats')
+            v-card-text
+              p Click the Stats Panel to view different performance metrics:
+              p.statsjs(ref='stats' @click='updateStatsDescription')
+              p(v-if='statsMode === 0') <strong>FPS</strong>: Frames rendered in the last second. The higher the number the better.
+              p(v-if='statsMode === 1') <strong>MS</strong>: Milliseconds needed to render a frame. The lower the number the better.
+              p(v-if='statsMode === 2') <strong>MB</strong>: MBytes of allocated memory
         
           v-card(light)
             v-card-title
               h2 Quick Settings
             v-card-text
+              p Selecting any of the following will overwrite your current settings.
               v-radio-group(v-model='quickSetting')
                 v-radio(label='Low accuracy; High performance' value='low')
                 v-radio(label='Balanced' value='balanced')
                 v-radio(label='High accuracy; Low performance' value='high')
                 v-radio(label='Custom' value='custom')
+              v-divider
               v-checkbox(label='Multi-user (beta)?' v-model='isMultiUser')
               v-checkbox.mt-0(label='Use animated background?' v-model='useBackground')
 </template>
@@ -38,7 +47,8 @@ export default {
     return {
       useBackground: true,
       isMultiUser: false,
-      quickSetting: 'custom'
+      quickSetting: 'custom',
+      statsMode: 0
     }
   },
 
@@ -58,6 +68,9 @@ export default {
   },
 
   methods: {
+    /**
+     * Toggle the Boids background animation
+     */
     toggleBackground () {
       const plugin = window.handsfree.plugin['boids-debugger']
       if (this.useBackground) {
@@ -65,6 +78,14 @@ export default {
       } else {
         plugin.disable()
       }
+    },
+
+    /**
+     * Update the stats description
+     */
+    updateStatsDescription () {
+      this.statsMode++
+      if (this.statsMode > 2) this.statsMode = 0
     }
   }
 }
