aafc44282b88014b7ff7e0b945b0f7a3a8611ef0
Oz Ramos <labofoz@gmail.com>
2018-12-21 12:06:31 -0800

    #4 Basic calculateXY test

--- handsfree.js/components/Cursor.test.js
+++ handsfree.js/components/Cursor.test.js
@@ -8,4 +8,18 @@ describe('Handsfree.prototype.injectCursor', () => {
     handsfree._injectCursor()
     expect(document.querySelector('.handsfree-cursor')).toBeTruthy()
   })
+
+  it('calculates X/Y', () => {
+    const handsfree = new Handsfree()
+    handsfree.brf = {sdk: {BRFState: {FACE_TRACKING: -1}}}
+    handsfree._injectCursor()
+
+    // Test looking straight ahead
+    handsfree.faces = [{
+      rotationX: 0,
+      rotationY: 0
+    }]
+    handsfree._calculateXY()
+    expect(handsfree.faces[0].cursor).toEqual({x: window.innerWidth / 2, y: window.innerHeight / 2})
+  })
 })
