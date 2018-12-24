4a3df29d798fb3753d93b85c8bc79c39b3e4c0c6
Oz Ramos <labofoz@gmail.com>
2018-12-23 18:39:00 -0800

    #43 describe SimpleKeyboard.show, cleaned up code

--- handsfree.js/plugins/SimpleKeyboard.test.js
+++ handsfree.js/plugins/SimpleKeyboard.test.js
@@ -1,34 +1,58 @@
-jest.useFakeTimers()
-jest.mock('simple-keyboard')
-const Plugin = require('./SimpleKeyboard')
-let handsfree
-let plugin
-
+/**
+ * - Restores: .on, .dispatch
+ * - Fast forward to plugin onUse()
+ * - Adds a fake $target input
+ */
 beforeEach(() => {
+  // Fresh testing components
   handsfree = new Handsfree()
   plugin = Plugin
 
+  // Add fake $target
+  $target = document.createElement('input')
+  $target.setAttribute('type', 'text')
+  document.body.appendChild($target)
+  plugin.$target = $target
+
+  // Fake click event
+  clickEvent = new MouseEvent('click', {
+    target: $target    
+  })
+
+  // Restore methods and fast forward
   Handsfree._mock.restore(handsfree, 'on')
+  Handsfree._mock.restore(handsfree, 'dispatch')
   handsfree._use(plugin)
   plugin.$handsfree = handsfree
   jest.runAllTimers()
 })
 
+/**
+ * SimpleKeyboard.onUse
+ * - The `handsfree:SimpleKeyboard:injectKeyboard` event is important for developers
+ */
 describe('SimpleKeyboard.onUse', () => {
   it('dispatches SimpleKeyboard:injectKeyboard', () => {
     const cb = jest.fn()
     window.addEventListener('handsfree:SimpleKeyboard:injectKeyboard', cb)
-    Handsfree._mock.restore(handsfree, 'dispatch')
 
     plugin.onUse()
     expect(cb).toHaveBeenCalled()
-
-    window.removeEventListener('handsfree:SimpleKeyboard:injectKeyboard', cb)
   })
 })
 
+/**
+ * SimpleKeyboard.show
+ * - Triggered via `$handsfree.on('SimpleKeyboard:show')`
+ */
 describe('SimpleKeyboard.show', () => {
-  it('adds body class and sets text to match target input', () => {})
+  it('adds body class and sets text to match target input', () => {
+    $target.value = 'hello motto'
+    handsfree.dispatch('SimpleKeyboard:show')
+
+    expect(document.body.classList).toContain('handsfree-simple-keyboard-is-visible')
+    expect(plugin.$target.value).toBe('hello motto')
+  })
 })
 
 describe('SimpleKeyboard.hide', () => {
@@ -49,3 +73,12 @@ describe('SimpleKeyboard.listenToFocusEvents', () => {
   it('responds to click and focusin events', () => {})
   it('affects input[type="text"]', () => {})
 })
+
+// Spare parts
+jest.useFakeTimers()
+jest.mock('simple-keyboard')
+const Plugin = require('./SimpleKeyboard')
+let handsfree
+let plugin
+let $target
+let clickEvent
