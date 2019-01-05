73c8b3597cf34ffcce1449f2c8989f3e29cba1b7
Oz Ramos <labofoz@gmail.com>
2019-01-05 10:13:50 -0800

    #52 Unretired Home-v1, updated routes

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SpaceWhales from '../components/space-whales/Landing.vue'
 import Home from '../components/home/Landing.vue'
-import HomeV1 from '../components/_retired/Home-v1.vue'
 import YouTubeLanding from '../components/youtube/Landing.vue'
 import YouTubeSingle from '../components/youtube/Single.vue'
 import HolodeckLanding from '../components/holodeck/Landing.vue'
@@ -11,11 +11,11 @@ Vue.use(VueRouter)
 export default new VueRouter({
   scrollBehavior () {return {x: 0, y: 0}},
   routes: [
-    {name: 'Home', path: '/', component: HomeV1},
+    {name: 'Home', path: '/', component: Home},
     {name: 'settings', path: '/settings', component: Settings},
     {name: 'youtubeLanding', path: '/youtube', component: YouTubeLanding},
     {name: 'youtubeSingle', path: '/youtube/:id', component: YouTubeSingle},
     {name: 'holodeckLanding', path: '/holodeck', component: HolodeckLanding},
-    {name: 'spaceWhalesLanding', path: '/space-whales', component: Home}
+    {name: 'spaceWhalesLanding', path: '/space-whales', component: SpaceWhales}
   ]
 })
