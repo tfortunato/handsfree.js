136580bd1121bb5eae26ee9fc4975d156fe5856f
Oz Ramos <labofoz@gmail.com>
2019-01-22 23:40:21 -0500

    #53 /docs/quickstart

--- src/components/docs/Landing.vue
+++ src/components/docs/Landing.vue
@@ -9,7 +9,8 @@ div
           v-card-title
             h1 Intro
           v-card-text
-            p <strong>Handsfree.js</strong> is a drop-in library that adds head tracked cursors in just a few lines. By default, these cursors come with several plugins to enable clicking, typing, and page scrolling with no setup!
+            p <strong>Handsfree.js</strong> is a drop-in library that adds head tracked cursors in just a few lines. By default, <strong>Handsfree.js</strong> is configured for web browsing: clicking, typing, and page scrolling all work out the box with no setup!
+            p To calculate where to place these cursors, <strong>Handsfree.js</strong> uses computer vision libraries like <a href="https://github.com/Tastenkunst/brfv4_javascript_examples">BRFv4</a> and <a href="https://github.com/tensorflow/tfjs-models/tree/master/posenet">PoseNet</a> to extract pose information.
             p.text-xs-center
               img(src='https://media.giphy.com/media/7JmuMG4jX2I3fV8XvT/source.gif')
             p.text-xs-center 
--- src/components/docs/Quickstart.vue
+++ src/components/docs/Quickstart.vue
@@ -6,19 +6,55 @@ div
 
       v-flex(xs12 md8 lg9)
         v-card
-          v-card-title
-            h1 Quickstart
+          v-card-title(primary-title)
+            h2.headline.mb-0 Quickstart
           v-card-text
-            p <strong>Handsfree.js</strong> is a drop-in library that adds head tracked cursors in just a few lines. By default, these cursors come with several plugins to enable clicking, typing, and page scrolling with no setup!
-            p.text-xs-center
-              img(src='https://media.giphy.com/media/7JmuMG4jX2I3fV8XvT/source.gif')
-            p.text-xs-center 
-              a(href='https://handsfree-drawing.glitch.me') Remix the Handsfree Drawing Starter Kit on Glitch.com
+            v-expansion-panel(v-model='tab')
+              v-expansion-panel-content
+                strong(slot='header') In HTML
+                v-card
+                  v-card-text
+                    p This is how we encourage you to use <strong>Handsfree.js</strong> when using standard web technologies or when experimenting on sites like <a href="https://glitch.com/~handsfree-starter">Glitch.com (check out the Handsfree Starter!)</a>:
+                    pre 
+                      code.xml.
+                        &lt;!-- Lock Handsfree.js to a specific version (RECOMMENDED) --&gt;
+                        &lt;script src="https://unpkg.com/handsfree@4.0.2/dist/handsfree.js">&lt;/script&gt;
+
+                        &lt;!-------------------- or --------------------&gt;
+                        &lt;!-- Use any new minor release before 5.0.0 --&gt;
+                        &lt;script src="https://unpkg.com/handsfree@&lt;5/dist/handsfree.js">&lt;/script&gt;
+
+                        &lt;!-------------------- or --------------------&gt;
+                        &lt;!-- Use the very latest (NOT recommended) --&gt;
+                        &lt;script src="https://unpkg.com/handsfree/dist/handsfree.js">&lt;/script&gt;
+
+                        &lt;!------------------ then --------------------&gt;
+                        &lt;!-- Instantiate Handsfree with default settings and start it --&gt;
+                        &lt;script&gt;
+                          handsfree = new Handsfree()
+                          handsfree.start()
+                        &lt;/script>
+
+              v-expansion-panel-content
+                strong(slot='header') In Node
+                v-card
+                  v-card-text
+                    p When developing for the Internet of Things or non-browser based environments, we encourage you to use the below method:
+                    pre
+                      code.bash npm install handsfree
+
+                    div then:
+                    pre 
+                      code.javascript.
+                       import Handsfree from 'handsfree'
+                        const handsfree = new Handsfree()
+                        handsfree.start()
+
             v-flex
               v-btn(color='primary' :to='{name: "docsIntro"}')
                 v-icon chevron_left
                 | Intro
-              v-btn(color='primary' :to='{name: "docsQuickstart"}' style='float: right')
+              v-btn(color='primary' :to='{name: "docsConfig"}' style='float: right')
                 | Quickstart
                 v-icon chevron_right
               .clear
@@ -26,9 +62,22 @@ div
 
 <script>
 import DocsSidebar from './Sidebar'
+import hljs from 'highlight.js'
 
 export default {
   name: 'docsQuickstart',
-  components: {DocsSidebar}
+  components: {DocsSidebar},
+
+  mounted () {
+    window.hljs = hljs
+    hljs.initHighlighting.called = false
+    hljs.initHighlighting()
+  },
+
+  data () {
+    return {
+      tab: 0
+    }
+  }
 }
 </script>
